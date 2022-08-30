3b2cd989903c0435fa3fb688246171df6ac757f1
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2022-08-30 11:05:45 -0400

    init

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+youtuber = "Mihir Gupta"
+print("subscribe to " + youtuber)
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb :")
+famous = input("Famous Person: ")
+
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous}"
+
+print(madlib)
